0bed2a12a86b53d9862d5aca15f24aa1e6d112ea
karola-kasprzak <64924406+karola-kukowska@users.noreply.github.com>
2023-06-25 16:13:28 +0200

    home, about and error pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu
+              sem magna. Donec ut arcu auctor, efficitur neque sit amet, aliquet
+              arcu. Nullam rhoncus massa vel dignissim porttitor. Suspendisse
+              finibus suscipit nisi imperdiet accumsan. Duis nec orci vel ex
+              volutpat ornare.
+            </p>
+            <p>
+              Praesent accumsan id felis in consequat. Suspendisse non tempus
+              erat, ac pharetra enim. Quisque ac velit odio. Fusce vitae
+              elementum lacus. Cras eros erat, aliquet ac magna id, malesuada
+              sodales odio. Vivamus eget diam eget lacus pulvinar tempus. Ut
+              nunc neque, mollis nec ante feugiat, vehicula pretium erat.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +60,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
